19a934cc44caa377ec43e754302cd26bceeaaeb6
kingli <1258093480@qq.com>
2015-11-15 21:14:45 +0800

    静态的框架搭建

--- public/script/main.js
+++ public/script/main.js
@@ -2,15 +2,45 @@ $(document).ready(function () {
 	var ME = {
 		USE: {},
 		DOM: {
-			$contents: {}
+			$wrapper: $('#wrapper'),
+			$menus: $('#wrapper li .menu'),
+			$contents: $('#wrapper li .content'),
+			$menuTemplate: $('#menuListTemplate'),
+			$contentTemplate: $('#contentTemplate')
+		},
+		TMP: window.juicer,
+		METHODS: {}
+	};
+	ME.METHODS.BuildDom = function ($wrapper, $template) {
+		var buildTmp = ME.TMP($template.html());
+		return function (data, handle) {
+			$wrapper.append(buildTmp.render(data));
+			handle($wrapper);
 		}
 	};
-	var $contents = $('.content').masonry({
+	/*瀑布流设置*/
+	ME.DOM.$contents = $('.content').masonry({
 		itemSelector: '.item',
 		columnWidth: '.head',
 		percentPosition: true
 	});
-	$contents.imagesLoaded().progress(function () {
-		$contents.masonry('layout');
+	ME.DOM.$contents.imagesLoaded().progress(function () {
+		ME.DOM.$contents.masonry('layout');
 	});
+	(function init() {
+		var buildMenu = ME.METHODS.BuildDom(ME.DOM.$wrapper, ME.DOM.$menuTemplate);
+		$.getJSON('data/main.json')
+			.done(function (data) {
+				buildMenu(data, function ($wrapper) {
+					$wrapper.on('click', 'li .menu', function (event) {
+						/*隐藏或显示列表*/
+						$(this).next().toggle('slow');
+					});
+				});
+			})
+			.fail(function () {
+				console.log('数据有错！');
+			});
+
+	})();
 });
