3e7864d6ff1aa9ada8f54555087ef6f8ab6527e7
kingli <1258093480@qq.com>
2015-11-18 20:56:07 +0800

    jade模板的基本架构

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 var express = require('express'),
 	path = require('path');
 var port = process.env.VCAP_APP_PORT || 3000;
+var routes=require('./routes/index');
 var app = express();
 app.listen(port,function(){
 	console.log('the sever run in the localhost:'+port);
 });
-var routes=require('./routes/index');
-/*设置静态文件托管目录*/
+/*设置模板引擎*/
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-//app.use('/build', routes);
 
-// 404错误处理
+app.use('/', routes);
+
+
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-/*开发者调试用的*/
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-		console.log('开发时发生一些错误，啥子呢？',err);
-    });
-}
-app.use(function(err, req, res, next) {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-	console.log('生产环境发生一些错误，啥子呢？',err);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var app = express();
-app.get('/', function (req, res, next) {
-	return res.sendfile('./public/index.html');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
-app.route('/build').get(function (req, res) {}).post(function (req, res) {});
-module.exports = app;
+
+module.exports = router;
