7568cf2226de5806ae88672ee346994ce6767c34
kingli <1258093480@qq.com>
2015-11-19 10:22:04 +0800

    数据库测试

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,24 +1,17 @@
-var config={
-	"dbpath":"./public/data/data.db",
-
-	"createThemeTba": "CREATE TABLE IF NOT EXISTS  theme(" +
+var config = {
+	dbpath:"data.db",
+	createThemeTba: "CREATE TABLE IF NOT EXISTS  theme(" +
 		"id INTEGER PRIMARY KEY NOT NULL," +
 		"info TEXT NOT NULL," +
 		"date NOT NULL DEFAULT (datetime('now','localtime')))",
-
-
-	"createFilesTba": "CREATE TABLE IF NOT EXISTS files(" +
+	createFilesTba: "CREATE TABLE IF NOT EXISTS files(" +
 		"id INTEGER PRIMARY KEY NOT NULL," +
 		"sid INTEGER REFERENCES theme(id) ON UPDATE CASCADE," +
 		"type TEXT NOT NULL," +
 		"path TEXT NOT NULL)",
-
-
-	"insertTheme": "INSERT INTO theme(info)VALUES(?)",
-	"selectTheme": "SELECT * FROM theme",
-
-	"insertFilse": "INSERT INTO files(sid,type,path)VALUES(?,?,?)",
-	"selectFiles": "SELECT * FROM files where id=?"
-
-};
+	insertTheme: "INSERT INTO theme(info)VALUES(?)",
+	selectTheme: "SELECT * FROM theme",
+	insertFilse: "INSERT INTO files(sid,type,path)VALUES(?,?,?)",
+	selectFiles: "SELECT * FROM files where id=?"
+}
 module.exports = config;
--- dbctrl/main.js
+++ dbctrl/main.js
@@ -1,38 +1,28 @@
 var CONFIG = require('../config/dbconfig.js');
+console.log(CONFIG);
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(CONFIG.dbpath),
-	prepareInsetTheme = db.prepare(config.insertTheme),
-	prepareSelectTheme = db.prepare(config.selectTheme),
-	prepareInsetFiles = db.prepare(config.insertFilse),
-	prepareSelectFiles = db.prepare(config.selectFiles);
+	prepareInsetTheme = db.prepare(CONFIG.insertTheme),
+	prepareSelectTheme = db.prepare(CONFIG.selectTheme),
+	prepareInsetFiles = db.prepare(CONFIG.insertFilse),
+	prepareSelectFiles = db.prepare(CONFIG.selectFiles);
+var log = {
+	begin: function (info) {
+		console.log('begin to:' + info);
+	},
+	end: function (info) {
+		console.log('complete the:' + info);
+	}
+}
 var dbctrl = {
 	init: function () {
 		db.serialize(function () {
-			db.run(config.createThemeTba);
-			db.run(config.createFilesTba);
-
+			log.begin('create table');
+			db.run(CONFIG.createThemeTba);
+			db.run(CONFIG.createFilesTba);
 		});
 		db.close();
 	},
-	insertTheme: function (data) {
-		db.serialize(function () {
-			prepareInsetTheme.run(data);
-		});
-		db.close();
-	},
-	selectTheme:function(data){
-		db.serialize(function () {
-			prepareInsetTheme.run(data,function(err,row){
-				if(err){
-					console.log('err:',err);
-				}
-				console.log(row);
-			});
-		});
-		db.close();
-	}
 };
 dbctrl.init();
-dbctrl.insertTheme("数据啦");
-//dbctrl.selectTheme();
-module.exports=dbctrl;
+module.exports = dbctrl;
